85681959e839f6584df61f93719d2a2f24fa444d
jaeseok <jsoh07@sharemind.kr>
2024-09-09 01:05:15 +0900

    main css

--- src/Main.js
+++ src/Main.js
@@ -2,105 +2,45 @@ import style from "./main.module.css";
 
 function Main(){
     return (
-        <main clssName={style.main}>
+        <main className={style.main}>
           <div>
             <div>
-              <section>
-                <div>
-                  <h2>빅배너</h2>
-                  <div>
-                    <span>1</span>
+              <section className={style.mainSection}>
+                <div className={style.multiBanner}>
+                  <h2 className={style.ir}>빅배너</h2>
+                  <div className={style.swiperPaginationFraction}>
+                    <span className={style.swiperPaginationCurrent}>1</span>
                     |
-                    <span>5</span>
+                    <span className={style.swiperPaginationTotal}>5</span>
                   </div>
-                  <button>이전배너</button>
-                  <button>다음배너</button>
-                  <div>
-                    <div>
+                  <button type="button" tabindex="0" aria-label="Next slide" className={style.swiperButtonPrev}>이전배너</button>
+                  <button type="button" tabindex="0" aria-label="Previous slide" className={style.swiperButtonNext}>다음배너</button>
+                  <div className={style.swiperWrapper}>
+                    <div className={style.swiperSlide}>
                       <div>
                         <picture></picture>
                         <picture></picture>
                       </div>
                     </div>
-                    <div>
+                    <div className={style.swiperSlide}>
                       <div>
                         <picture></picture>
                         <picture></picture>
                       </div>
                     </div>
-                    <div>
+                    <div className={style.swiperSlide}>
                       <div>
                         <picture></picture>
                         <picture></picture>
                       </div>
                     </div>
-                    <div>
+                    <div className={style.swiperSlide}>
                       <div>
                         <picture></picture>
                         <picture></picture>
                       </div>
                     </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
-                      <div>
-                        <picture></picture>
-                        <picture></picture>
-                      </div>
-                    </div>
-                    <div>
+                    <div className={style.swiperSlide}>
                       <div>
                         <picture></picture>
                         <picture></picture>
