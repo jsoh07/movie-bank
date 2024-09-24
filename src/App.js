91ebf3a8a56e5076141b0fbdeca90f919bc4bd96
jaeseok <jsoh07@sharemind.kr>
2024-09-24 16:37:34 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home.js";
 import MovieInfo from "./component/MovieInfo.js";
 import Search from "./component/Search.js";
 import Category from "./component/Category.js";
 
-function App(){
-    return(
-        <div>
+function App() {
+    return (
+        <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/movie/:id" element={<MovieInfo />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/category/:id" element={<Category />} />
             </Routes>
-        </div>
-        
+        </Router>
     );
 }
 
